bbcbaff3f6a8baad48f76b266d857c817fc6278b
Thomas Marks <marksta@umich.edu>
2021-09-08 17:07:23 -0400

    test for more H9-like geometry

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 function test_many_loops()
 	xs = -0.4:0.005:0.4
-	ys = -0.4:0.005:0.4
+	ys = 0.0:0.0
 	zs = -0.3:0.005:0.3
 
 	angles = 0:π/3:5π/3
@@ -13,15 +13,10 @@ function test_many_loops()
 	loop_radius = 0.04
 	loop_current = 2 * loop_radius * field_strength / LoopFieldCalc.μ₀
 
-	#outer_loops =
-	#	[LoopFieldCalc.CurrentLoop(loop_radius, loop_current, LoopFieldCalc.CylindricalPoint(-0.01, r2, θ)) for θ in angles]
+	outer_loop = LoopFieldCalc.CurrentLoop(0.28, 1/5*loop_current, LoopFieldCalc.CylindricalPoint(0.0, 0.0, 0.0))
+	inner_loop = LoopFieldCalc.CurrentLoop(0.1, 2*loop_current, LoopFieldCalc.CylindricalPoint(0.0, 0.0, 0.0))
 
-	inner_loops =
-		[LoopFieldCalc.CurrentLoop(loop_radius, -loop_current, LoopFieldCalc.CylindricalPoint(-0.01, r1, θ)) for θ in angles]
-
-	#loops = [outer_loops; inner_loops]
-
-	Bx, By, Bz = LoopFieldCalc.field_on_box(inner_loops, xs, ys, zs)
+	Bx, By, Bz = LoopFieldCalc.field_on_box([outer_loop, inner_loop], xs, ys, zs)
 	LoopFieldCalc.write_field("many_loops.dat", xs, ys, zs, Bx, By, Bz)
 end
 
